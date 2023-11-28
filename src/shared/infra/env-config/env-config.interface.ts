94569f2ffdc3ea4197d49c7cb5ce96936f276820
Jean Osco <willjean29@gmail.com>
2023-11-28 00:17:58 -0500

    feat(env): implementing envvonfig interface

--- src/shared/infra/env-config/env-config.interface.ts
+++ src/shared/infra/env-config/env-config.interface.ts
@@ -2,3 +2,8 @@ export interface EnvConfig {
   getAppPort(): number;
   getNodeEnv(): string;
 }
+
+export enum EnvNames {
+  Port = 'PORT',
+  NodeEnv = 'NODE_ENV',
+}
--- src/shared/infra/env-config/env-config.service.ts
+++ src/shared/infra/env-config/env-config.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { EnvConfig, EnvNames } from './env-config.interface';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>(EnvNames.Port));
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get<string>(EnvNames.NodeEnv);
+  }
+}
