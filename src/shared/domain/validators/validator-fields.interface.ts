3c238f2e210b4046736a2917f9f855f9ca2e4c27
Jean Osco <willjean29@gmail.com>
2023-11-30 13:28:00 -0500

    feat(shared): add abstract class 'ClassValidatorFields'

--- src/shared/domain/validators/validator-fields.interface.ts
+++ src/shared/domain/validators/validator-fields.interface.ts
@@ -2,7 +2,7 @@ export type FieldsErrors = {
   [field: string]: string[];
 };
 
-export interface ValidatorsFieldsInterface<PropsValidated> {
+export interface ValidatorFieldsInterface<PropsValidated> {
   errors: FieldsErrors;
   validatedData: PropsValidated;
   validate(data: any): boolean;
