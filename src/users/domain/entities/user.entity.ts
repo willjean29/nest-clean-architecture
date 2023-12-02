f121f27100aa3654c388b97d3e1e6cc23e582107
Jean Osco <willjean29@gmail.com>
2023-12-02 00:17:18 -0500

    feat(users): add validator user implementations

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Entity } from '../../../shared/domain/entities/entity';
+import { UserValidatorFactory } from '../validators/user.validator';
 
 export type UserProps = {
   name: string;
@@ -12,15 +13,18 @@ export class UserEntity extends Entity<UserProps> {
     public readonly props: UserProps,
     id?: string,
   ) {
+    UserEntity.validate(props);
     super(props, id);
     this.props.createdAt = this.props.createdAt ?? new Date();
   }
 
   updateName(name: string): void {
+    UserEntity.validate({ ...this.props, name });
     this.name = name;
   }
 
   updatePassword(password: string): void {
+    UserEntity.validate({ ...this.props, password });
     this.password = password;
   }
 
@@ -46,5 +50,10 @@ export class UserEntity extends Entity<UserProps> {
 
   private set password(password: string) {
     this.props.password = password;
+  }
+
+  static validate(props: UserProps) {
+    const validator = UserValidatorFactory.create();
+    validator.validate(props);
   }
 }
---
+++ src/users/domain/validators/user.validator.ts
@@ -0,0 +1,40 @@
+import { IsDate, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+import { UserProps } from '../entities/user.entity';
+import { ClassValidatorFields } from '../../../shared/domain/validators/class-validator-fields';
+
+class UserRules {
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  email: string;
+
+  @MaxLength(100)
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsDate()
+  @IsOptional()
+  createdAt?: Date;
+
+  constructor({ name, email, password, createdAt }: UserProps) {
+    Object.assign(this, { name, email, password, createdAt });
+  }
+}
+
+export class UserValidator extends ClassValidatorFields<UserRules> {
+  validate(data: UserProps) {
+    return super.validate(new UserRules(data ?? ({} as UserProps)));
+  }
+}
+
+export class UserValidatorFactory {
+  static create(): UserValidator {
+    return new UserValidator();
+  }
+}
