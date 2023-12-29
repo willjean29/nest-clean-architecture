a524759b2841b74c17594c99be29d0c77606cb48
Jean Osco <willjean29@gmail.com>
2023-12-28 22:16:25 -0500

    test(users): add integration test - validate password field on create user

--- src/users/domain/__tests__/integration/user.entity.int.spec.ts
+++ src/users/domain/__tests__/integration/user.entity.int.spec.ts
@@ -43,5 +43,25 @@ describe('UserEntity integration tests', () => {
       };
       expect(() => new UserEntity(props)).toThrow(EntityValidationError);
     });
+
+    it('Should throw an error when creating user with invalid password', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        password: null,
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: '',
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+
+      props = {
+        ...UserDataBuilder({}),
+        password: 'a'.repeat(256),
+      };
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    });
   });
 });
